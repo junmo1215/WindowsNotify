6b61c969e9411978b2580e577cd0d38f4fd5c6a1
junmolu <junmolu@futunn.com>
2021-04-27 13:16:07 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WindowsNotify
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string title = "title";
+        public static string msg = "msg";
+        public static int duration = 5;
+
+        private void ParseArgs(object sender, StartupEventArgs e)
+        {
+            for (int index = 0; index < e.Args.Length; index += 2)
+            {
+                if (e.Args.Length == index + 1 || e.Args[index + 1].StartsWith("-"))
+                {
+                    //args.Add(e.Args[index], string.Empty);
+                    index--;
+                }
+
+                if (e.Args.Length >= index + 1 && !e.Args[index + 1].StartsWith("-"))
+                {
+                    //args.Add(e.Args[index], e.Args[index + 1]);
+                    if (e.Args[index] == "-title")
+                    {
+                        title = e.Args[index + 1];
+                    }
+                    else if (e.Args[index] == "-msg")
+                    {
+                        msg = e.Args[index + 1];
+                    }
+                    else if (e.Args[index] == "-duration")
+                    {
+                        if (!Int32.TryParse(e.Args[index + 1], out duration))
+                        {
+                            duration = 5;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsNotify
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            label_title.Content = App.title;
+            label_msg.Content = App.msg;
+
+            this.Left = SystemParameters.PrimaryScreenWidth - this.Width;
+            this.Top = SystemParameters.PrimaryScreenHeight - this.Height;
+
+            this.Loaded += WindowReady;
+        }
+
+        private void WindowReady(object sender, RoutedEventArgs e)
+        {
+            MainWindow self = sender as MainWindow;
+            self.UpdateLayout();
+
+            double top = SystemParameters.PrimaryScreenHeight - self.Height;
+            double bottom = SystemParameters.PrimaryScreenHeight;
+            int animation_duration = 500;
+
+            Task.Factory.StartNew(delegate
+            {
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(App.duration));
+                this.Dispatcher.Invoke(delegate
+                {
+                    DoubleAnimation animation = new DoubleAnimation();
+                    animation.Duration = new Duration(TimeSpan.FromMilliseconds(animation_duration));
+                    animation.From = top;
+                    animation.To = bottom;
+                    animation.Completed += Exit;
+                    self.BeginAnimation(Window.TopProperty, animation);
+                });
+            });
+        }
+
+        /// <summary>
+        /// 退出按钮点击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_close_Click(object sender, RoutedEventArgs e)
+        {
+            Exit(sender, e);
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
